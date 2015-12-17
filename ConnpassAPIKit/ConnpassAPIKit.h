6b26ce364451521c48995d8725108c8f8ccacd52
JPMartha <sathachie@gmail.com>
2015-12-17 13:34:36 +0900

    Create ConnpassAPIKit

---
+++ ConnpassAPIKit/ConnpassAPIKit.h
@@ -0,0 +1,19 @@
+//
+//  ConnpassAPIKit.h
+//  ConnpassAPIKit
+//
+//  Created by sathachie on 2015/12/17.
+//  Copyright © 2015年 JPMartha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnpassAPIKit.
+FOUNDATION_EXPORT double ConnpassAPIKitVersionNumber;
+
+//! Project version string for ConnpassAPIKit.
+FOUNDATION_EXPORT const unsigned char ConnpassAPIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnpassAPIKit/PublicHeader.h>
+
+
