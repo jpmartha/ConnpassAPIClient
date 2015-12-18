e359f967c05575971b11854b5727a8dab771d15c
JPMartha <sathachie@gmail.com>
2015-12-18 12:07:04 +0900

    Create ConnpassAPIClient framework

---
+++ ConnpassAPIClient/ConnpassAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  ConnpassAPIClient.h
+//  ConnpassAPIClient
+//
+//  Created by sathachie on 2015/12/18.
+//  Copyright © 2015年 JPMartha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnpassAPIClient.
+FOUNDATION_EXPORT double ConnpassAPIClientVersionNumber;
+
+//! Project version string for ConnpassAPIClient.
+FOUNDATION_EXPORT const unsigned char ConnpassAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnpassAPIClient/PublicHeader.h>
+
+
